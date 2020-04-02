df560c28b7cd0a3effcfe0fcf1d3673a2d49022d
Zviad Metreveli <zviad@dropbox.com>
2020-04-02 12:06:36 -0400

    Crash quorum tests using standard functions

--- walle/itest/crashing_quorum/crashing_quorum_test.go
+++ walle/itest/crashing_quorum/crashing_quorum_test.go
@@ -10,7 +10,6 @@ import (
 	topomgr_pb "github.com/zviadm/walle/proto/topomgr"
 	"github.com/zviadm/walle/tt/servicelib"
 	"github.com/zviadm/walle/walle/itest"
-	"github.com/zviadm/walle/walle/storage"
 	"github.com/zviadm/walle/walle/topomgr"
 	"github.com/zviadm/walle/wallelib"
 	"github.com/zviadm/zlog"
@@ -19,41 +18,21 @@ import (
 func TestCrashingQuorum(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
-	rootURI := topomgr.Prefix + "itest"
-	wDir := storage.TestTmpDir()
+	defer servicelib.KillAll(t)
+	defer servicelib.IptablesClearAll(t)
 
-	rootTopology := itest.BootstrapDeployment(t, ctx, rootURI, wDir, itest.RootDefaultPort)
-	s := make([]*servicelib.Service, 3)
-	s[0] = itest.RunWalle(t, ctx, rootURI, "", rootTopology, wDir, itest.RootDefaultPort)
-	defer s[0].Kill(t)
+	s, rootPb := itest.SetupRootNodes(t, ctx, 3)
 
-	rootD, err := wallelib.NewRootDiscovery(ctx, rootTopology)
+	cli, err := wallelib.NewClientFromRootPb(ctx, rootPb, "")
 	require.NoError(t, err)
-	cli := wallelib.NewClient(ctx, rootD)
 	topoMgr := topomgr.NewClient(cli)
-
-	var serverIds []string
-	for i := 1; i <= 2; i++ {
-		s[i] = itest.RunWalle(t, ctx, rootURI, "", rootTopology, storage.TestTmpDir(), itest.RootDefaultPort+i)
-		defer s[i].Kill(t)
-		topology, err := topoMgr.FetchTopology(ctx, &topomgr_pb.FetchTopologyRequest{TopologyUri: rootURI})
-		require.NoError(t, err)
-		serverIds = itest.ServerIdsSlice(topology.Servers)
-		_, err = topoMgr.UpdateServerIds(ctx, &topomgr_pb.UpdateServerIdsRequest{
-			TopologyUri: rootURI,
-			StreamUri:   rootURI,
-			ServerIds:   serverIds,
-		})
-		require.NoError(t, err)
-	}
 	_, err = topoMgr.UpdateServerIds(ctx, &topomgr_pb.UpdateServerIdsRequest{
-		TopologyUri: rootURI,
+		TopologyUri: rootPb.RootUri,
 		StreamUri:   "/t1/blast",
-		ServerIds:   serverIds,
+		ServerIds:   itest.ServerIdsSlice(rootPb.Servers),
 	})
 	require.NoError(t, err)
 
-	defer servicelib.IptablesClearAll(t)
 	crashWG := sync.WaitGroup{}
 	crashWG.Add(1)
 	crashC := make(chan time.Duration)
