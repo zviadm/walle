83b265e4701840ae26d6170bd05440aa7a87a52d
Zviad Metreveli <zviad@dropbox.com>
2020-04-06 12:53:11 -0400

    benchmark highcpu-4 nodes, 2 vcpus is way too small

--- benchops/create_cluster.sh
+++ benchops/create_cluster.sh
@@ -3,13 +3,11 @@ set -ex
 set -o pipefail
 
 IMAGEFAMILY="ubuntu-1804-lts"
-MACHINETYPE="n1-highcpu-2"
-
 gcloud compute instances create wctl-0 \
-	--image-project="ubuntu-os-cloud" --image-family=$IMAGEFAMILY --machine-type=$MACHINETYPE
+	--image-project="ubuntu-os-cloud" --image-family=$IMAGEFAMILY --machine-type="n1-highcpu-2"
 for i in {0..2}; do
 	gcloud compute instances create wnode-$i \
-		--image-project="ubuntu-os-cloud" --image-family=$IMAGEFAMILY --machine-type=$MACHINETYPE \
+		--image-project="ubuntu-os-cloud" --image-family=$IMAGEFAMILY --machine-type="n1-highcpu-4" \
 		--local-ssd interface="nvme"
 done
 
