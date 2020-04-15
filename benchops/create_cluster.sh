4c295da4307d9b7030d581afdb42c666bad2f5d2
Zviad Metreveli <zviad@dropbox.com>
2020-04-15 14:14:42 -0400

    [benchops] disable wctl creation

--- benchops/create_cluster.sh
+++ benchops/create_cluster.sh
@@ -7,17 +7,18 @@ SCRIPTDIR=$(dirname "$SCRIPTPATH")
 source "$SCRIPTDIR/datadog.key"
 
 IMAGEFAMILY="ubuntu-1804-lts"
-gcloud compute instances create wctl-0 \
-	--image-project="ubuntu-os-cloud" --image-family=$IMAGEFAMILY --machine-type="n1-highcpu-2"
+
+# gcloud compute instances create wctl-0 \
+# 	--image-project="ubuntu-os-cloud" --image-family=$IMAGEFAMILY --machine-type="n1-highcpu-2"
+# gcloud compute ssh wctl-0 -- "
+# env DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$DD_API_KEY bash -c \"\$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)\"
+# "
+
 for i in {0..2}; do
 	gcloud compute instances create wnode-$i \
 		--image-project="ubuntu-os-cloud" --image-family=$IMAGEFAMILY --machine-type="n1-highcpu-4" \
 		--local-ssd interface="nvme"
 done
-
-gcloud compute ssh wctl-0 -- "
-env DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$DD_API_KEY bash -c \"\$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)\"
-"
 for i in {0..2}; do
 	gcloud compute ssh wnode-$i -- "
 sudo mkfs.ext4 -F /dev/nvme0n1
